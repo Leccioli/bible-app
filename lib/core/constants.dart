a74fd83f0f0056d4ee9daa5c245e948601ad7f1f
Vinicius Leccioli <viniciusleccioli@hotmail.com>
2026-03-10 15:23:37 -0300

    feat: add AppConstants class for API configuration and default values

---
+++ lib/core/constants.dart
@@ -0,0 +1,12 @@
+class AppConstants {
+  AppConstants._();
+
+  /// Base URL for the Bible API
+  static const String apiBaseUrl = 'https://rest.api.bible';
+
+  /// King James Version (KJV) Bible ID
+  static const String defaultBibleId = 'de4e12af7f28fbbf-02';
+
+  /// Default Bible version name displayed in the app
+  static const String defaultBibleName = 'King James (Authorized) Version';
+}
